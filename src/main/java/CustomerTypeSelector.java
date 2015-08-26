87561f6d1ef56c8404006a18a62b555c79cd56cc
sedighe <bit.mohebbi@gmail.com>
2015-08-26 10:29:20 +0430

    commit

---
+++ src/main/java/CustomerTypeSelector.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Dotin School1 on 8/26/2015.
+ */
+public class CustomerTypeSelector {
+}
---
+++ src/main/java/ParametrReader.java
@@ -0,0 +1,52 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ParametrReader extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head lang=\"en\">\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>customer input form</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "<div>\n" +
+                "    <div><h1> customer input form</h1></div>\n" +
+                "\n" +
+                "    <div>\n");
+        String customerType = request.getParameter("customerType");
+        boolean submited = Boolean.parseBoolean(request.getParameter("submited"));
+        if (submited) {
+            if (customerType == null) {
+                out.println("<p> select customer Type </p>");
+            } else if (!"real".equals(customerType) || !"legal".equals(customerType)) {
+
+                out.println("<p>invalid customer Type</p>");
+            }
+        }
+        out.println(
+                "        <form method=\"get\" action=\"http://localhost:9090/CustomerTypeSelector\">\n" +
+                        "            <fieldset>\n" +
+                        "                <input type=\"radio\" name=\"customerType\" value=\"real\" id=\"realCustomerType\" checked/>\n" +
+                        "                <label for=\"realCustomerType\">Real Customer</label>\n" +
+                        "                <input type=\"radio\" name=\"customerType\" value=\"legal\" id=\"legalCustomerType\"/>\n" +
+                        "                <label for=\"legalCustomerType\">Legal Customer </label>\n" +
+                        "            </fieldset>\n" +
+                        "            <input type=\"submit\" value=\"ok\"/>" +
+                        "<input type=\"hidden\" name=\"submited\" value=\"true\"/>");
+
+        out.println(" </form>\n" +
+                "    </div>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+    }
+
