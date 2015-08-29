f8d0a70db0041392e287132be69a3f5ad3d42df4
sedighe <bit.mohebbi@gmail.com>
2015-08-29 14:21:05 +0430

    commit

--- src/main/java/RealCustomer.java
+++ src/main/java/RealCustomer.java
@@ -1,4 +1,48 @@
 
-public class RealCustomer {
+public class RealCustomer extends Customer {
+    private String nationalCode;
+    private String birthDay;
+    private String fatherName;
+    private String lastName;
+    private String firstName;
 
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
